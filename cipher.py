f5094b708cb3c8ed432a53df5936f568b4eb24ee
Jordan White <jordanwhite268@gmail.com>
2023-11-05 19:58:01 -0500

    added dictionary

--- cipher.py
+++ cipher.py
@@ -1,1 +1,32 @@
 # add your code here
+sentence: str(input("Please enter a sentence:" )
+right_shift_5 = {
+    "a":"f",
+    "b":"g",
+    "c":"h",
+    "d":"i",
+    "e":"j",
+    "f":"k",
+    "g":"l",
+    "h":"m",
+    "i":"n",
+    "j":"o",
+    "k":"p",
+    "l":"q",
+    "m":"r",
+    "n":"s",
+    "o":"t",
+    "p":"u",
+    "q":"v",
+    "r":"w",
+    "s":"x",
+    "t":"y",
+    "u":"z",
+    "v":"a",
+    "w":"b",
+    "x":"c",
+    "y":"d",
+    "z":"e" }
+   print(shift_right_5["d"])
+              
+    
